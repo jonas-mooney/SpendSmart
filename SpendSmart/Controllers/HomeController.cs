ae7a03a49d6455ce794a4c9bd316622c46aef0ec
Jonas <jonasmooney2@gmail.com>
2025-07-03 16:32:15 -0600

    Add project files.

---
+++ SpendSmart/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SpendSmart.Models;
+
+namespace SpendSmart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Expenses()
+        {
+            return View();
+        }
+
+        // HomeController corresponds to Home view folder which has both Index and Privacy.
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
+// Following tutorial https://www.youtube.com/watch?v=xuFdrXqpPB0
+// Left off at 14:15
